576c08e7ab0e7196ffdeb1a11759426e06ca7b83
Protim Roy <protimr16@gmail.com>
2017-06-12 11:32:41 +0530

    Second commit

---
+++ frappe-bench/apps/mynewapp2/mynewapp2/random/doctype/naya_doctype/naya_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, RoyProtim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Naya Doctype', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ frappe-bench/apps/mynewapp2/mynewapp2/random/doctype/naya_doctype_part_2/naya_doctype_part_2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, RoyProtim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Naya Doctype part 2', {
+	refresh: function(frm) {
+
+	}
+});
